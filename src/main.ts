210602d0adfd51325eea260cbdc57b6ba8f8ccf1
mariasza <maria_araujo_souza@hotmail.com>
2025-07-23 17:25:12 -0400

    chore(swagger): configure SwaggerModule in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformResponseInterceptor } from './shared/interceptors/transform-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new TransformResponseInterceptor(reflector));
+
+  const config = new DocumentBuilder()
+    .setTitle('Rural Producer API')
+    .setDescription(
+      'API for managing rural producers, cultures, farms, and more.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
